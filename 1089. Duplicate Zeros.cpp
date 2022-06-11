5018a54edb8b8c3d2c7009a53a64b32cf5940295
Ankush <ankush100mit@gmail.com>
2022-06-12 00:21:15 +0530

    Create 1089. Duplicate Zeros.cpp

---
+++ 1089. Duplicate Zeros.cpp
@@ -0,0 +1,20 @@
+1089. Duplicate Zeros
+
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        vector<int>res;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==0){
+                res.push_back(0);
+                res.push_back(0);
+            }
+            else{
+                res.push_back(arr[i]);
+            }
+        }
+        for(int i=0;i<arr.size();i++){
+            arr[i]=res[i];
+        }
+    }
+};
