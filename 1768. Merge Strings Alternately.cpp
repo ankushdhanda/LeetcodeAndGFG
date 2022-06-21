ea302530f5c4dd247053f9f1fa5afe0eab02a65d
Ankush <ankush100mit@gmail.com>
2022-06-22 00:09:22 +0530

    Create 1768. Merge Strings Alternately.cpp

---
+++ 1768. Merge Strings Alternately.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int l1=word1.length();
+        int l2=word2.length();
+        int i=0,j=0;
+        string res="";
+        while(i<l1 && j<l2){
+            res+=word1[i++];
+            res+=word2[j++];
+        }
+        while(i<l1){
+            res+=word1[i++];
+        }
+        while(j<l2){
+            res+=word2[j++];
+        }
+        return res;
+    }
+};
