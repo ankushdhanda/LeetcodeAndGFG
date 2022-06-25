e8f9be9cd77448047fa5f52919cf8958b100d9ec
Ankush <ankush100mit@gmail.com>
2022-06-25 23:22:58 +0530

    Create 392. Is Subsequence.cpp

---
+++ 392. Is Subsequence.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int idx=0;
+        for(int i=0;i<t.length() && idx<s.length();i++){
+            if(s[idx]==t[i]){
+                idx++;
+            }
+        }
+        return idx==s.length();
+    }
+};
