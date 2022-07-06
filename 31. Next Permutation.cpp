4b497e9ed0f899c52fcee55c3b3f7b5087feb753
Ankush <ankush100mit@gmail.com>
2022-07-07 00:41:51 +0530

    Create 31. Next Permutation.cpp

---
+++ 31. Next Permutation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int idx1=-1;
+        int n=nums.size();
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx1=i;
+                break;
+            }
+        }
+        int idx2=0;
+        for(int i=n-1;i>=0;i--){
+            if(idx1>=0 && nums[i]>nums[idx1]){
+                idx2=i;
+                break;
+            }
+        }
+        if(idx1>=0)
+            swap(nums[idx1],nums[idx2]);
+        reverse(nums.begin()+idx1+1,nums.end());
+        
+    }
+};
